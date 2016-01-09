4cb3e4d83fcb0d214ad7b7e7fe8627cf185d2407
crossbow <crossbow@users.noreply.github.com>
2016-01-09 09:10:35 +0100

    Elixir template

---
+++ elixir.ex
@@ -0,0 +1,15 @@
+# module_name.ex
+defmodule ModuleName do
+
+  @moduledoc """
+  ModuleName documentation.
+  """
+  
+  @doc """
+  hello documentation.
+  """
+  def hello do
+    IO.puts "Hello, World!"
+  end
+  
+end
